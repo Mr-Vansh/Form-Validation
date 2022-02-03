d84339717676d5d6a5dab6ede41ff2c4de71c984
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2022-02-03 14:10:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let flag = 1;
+
+function formValidation() {
+    if (username.value == "") {
+        document.getElementById("userError").innerHTML =
+            "* Invalid Username *";
+        flag = 0;
+    } else {
+        document.getElementById("userError").innerHTML = "";
+        flag = 1;
+    }
+
+    if (password.value == "") {
+        document.getElementById("passError").innerHTML =
+            "* Invalid Password *";
+        flag = 0;
+    } else if (password.value.length <= 5) {
+        document.getElementById("passError").innerHTML =
+            "Password must be 8 characters long.";
+        flag = 0;
+    } else {
+        document.getElementById("passError").innerHTML = "";
+        flag = 1;
+    }
+
+    if (flag) {
+        return true;
+    } else {
+        return false;
+    }
+}
