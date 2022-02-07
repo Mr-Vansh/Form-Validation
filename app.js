7b1635505edcc9cd8755091bc7772e3b03402dba
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2022-02-07 19:02:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let username = document.getElementById("username");
 let password = document.getElementById("password");
-let flag = 1;
+let flag = 0;
 
 function formValidation() {
     if (username.value == "") {
         document.getElementById("userError").innerHTML =
             "* Invalid Username *";
-        flag = 0;
+//         flag = 0;
     } else if(username.value.length < 3) {
         document.getElementById("userError").innerHTML = 
             "Username must contain minimum 3 characters.";
-        flag = 0;
+//         flag = 0;
     } else {
         document.getElementById("userError").innerHTML = "";
         flag = 1;
@@ -19,17 +19,17 @@ function formValidation() {
     if (password.value == "") {
         document.getElementById("passError").innerHTML =
             "* Invalid Password *";
-        flag = 0;
+//         flag = 0;
     } else if (password.value.length <= 5) {
         document.getElementById("passError").innerHTML =
             "Password must be 8 characters long.";
-        flag = 0;
+//         flag = 0;
     } else {
         document.getElementById("passError").innerHTML = "";
         flag = 1;
     }
 
-    if (flag) {
+    if (flag==1) {
         alert("Form submitted successfully");
         return true;
     } else {
